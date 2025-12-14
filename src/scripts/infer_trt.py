23e2f397453a2984dbd3e35324b402a54f34a419
nevrohelios <dasharka05@gmail.com>
2025-12-14 21:01:48 +0000

    refactor:  TensorRT inference function

--- src/scripts/infer_trt.py
+++ src/scripts/infer_trt.py
@@ -1,25 +1,25 @@
-import tensorrt as trt
-import pycuda.driver as cuda
-import pycuda.autoinit
-import numpy as np
 import os
 from pathlib import Path
 from typing import Literal
 
-def infer_trt(mode: Literal['int8', 'fp16', 'int4'] = 'int8'):
+import numpy as np
+import pycuda.driver as cuda
+import tensorrt as trt
 
-    ENGINE_PATH = f"../../exports/model_{mode}_bs64.engine"  
+
+def infer_trt(
+    mode: Literal["int8", "fp16", "int4"] = "int8",
+    BATCH: int = 64,
+    NUM_CLASSES: int = 6,
+):
+    ENGINE_PATH = f"../../exports/model_{mode}_bs{BATCH}.engine"
     DATA_DIR = Path("../../data/CALIB")
     OUT_DIR = Path(f"../../data/TRT_OUT_{mode.upper()}")
     OUT_DIR.mkdir(exist_ok=True)
 
     assert os.path.exists(ENGINE_PATH), f"Engine file {ENGINE_PATH} does not exist."
 
-    BATCH = 64
-    NUM_CLASSES = 6  
-
     TRT_LOGGER = trt.Logger(trt.Logger.WARNING)
-
 
     with open(ENGINE_PATH, "rb") as f:
         engine = trt.Runtime(TRT_LOGGER).deserialize_cuda_engine(f.read())
@@ -32,18 +32,13 @@ def infer_trt(mode: Literal['int8', 'fp16', 'int4'] = 'int8'):
     d_input = cuda.mem_alloc(int(np.prod(input_shape)) * 4)
     d_output = cuda.mem_alloc(int(np.prod(output_shape)) * 4)
 
-    bindings = [int(d_input), int(d_output)]
     stream = cuda.Stream()
 
     def infer(x_np):
-        cuda.memcpy_htod_async(d_input, x_np, stream) # input -> device
+        cuda.memcpy_htod_async(d_input, x_np, stream)  # input -> device
 
-        context.set_tensor_address(
-            engine.get_tensor_name(0), int(d_input)
-        )
-        context.set_tensor_address(
-            engine.get_tensor_name(1), int(d_output)
-        )
+        context.set_tensor_address(engine.get_tensor_name(0), int(d_input))
+        context.set_tensor_address(engine.get_tensor_name(1), int(d_output))
 
         context.execute_async_v3(stream.handle)
 
@@ -62,7 +57,8 @@ def infer_trt(mode: Literal['int8', 'fp16', 'int4'] = 'int8'):
 
     print("TensorRT inference complete.")
 
+
 if __name__ == "__main__":
     # infer_trt(mode='int4')
     # infer_trt(mode='fp16')
-    infer_trt(mode='int8')
+    infer_trt(mode="int8")
